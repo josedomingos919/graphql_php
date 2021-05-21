cd88a87c833a0e44c164e50741d2eb4594864c1b
josedomingos919 <josedomingos919@gmail.com>
2021-05-21 12:46:36 +0100

    yep

---
+++ graph.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+// Test this using following command
+// php -S localhost:8080 ./graphql.php &
+// curl http://localhost:8080 -d '{"query": "query { echo(message: \"Hello World\") }" }'
+// curl http://localhost:8080 -d '{"query": "mutation { sum(x: 2, y: 2) }" }'
+require_once __DIR__ . './vendor/autoload.php';
+
+use GraphQL\GraphQL;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Schema;
+
+try {
+    $queryType = new ObjectType([
+        'name' => 'Query',
+        'fields' => [
+            'echo' => [
+                'type' => Type::string(),
+                'args' => [
+                    'message' =>Type::string(),
+                ],
+                'resolve' => static function ($rootValue, array $args): string {
+                    return "hello";
+                },
+            ],
+        ],
+    ]);
+
+    $mutationType = new ObjectType([
+        'name' => 'Calc',
+        'fields' => [
+            'sum' => [
+                'type' => Type::int(),
+                'args' => [
+                    'x' => ['type' => Type::int()],
+                    'y' => ['type' => Type::int()],
+                ],
+                'resolve' => static function ($calc, array $args): int {
+                    return $args['x'] + $args['y'];
+                },
+            ],
+        ],
+    ]);
+
+    // See docs on schema options:
+    // https://webonyx.github.io/graphql-php/type-system/schema/#configuration-options
+    $schema = new Schema([
+        'query' => $queryType,
+        'mutation' => $mutationType,
+    ]);
+
+    $rawInput       = file_get_contents('php://input');
+    $input          = json_decode($rawInput, true);
+    
+    $query          = $input['query'];
+    $variableValues = $input['variables'] ?? null;
+
+    $rootValue = ['prefix' => 'You said: '];
+    $result    = GraphQL::executeQuery($schema, $query, $rootValue, null, $variableValues);
+    $output    = $result->toArray();
+} catch (Throwable $e) {
+    $output = [
+        'error' => [
+            'message' => $e->getMessage(),
+        ],
+    ];
+}
+
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($output);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'webonyx/graphql-php' => 
+    array (
+      'pretty_version' => 'v14.7.0',
+      'version' => '14.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6b0ba9a4688b2ef74ea4d52b27477dd80fa67f31',
+    ),
+  ),
+);
